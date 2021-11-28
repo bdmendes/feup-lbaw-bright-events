c3dc1982d50d05ed820c5930ffeed63a7ef6bb9d
Bruno Mendes <brod56@sapo.pt>
2021-11-28 17:40:05 +0000

    Fix schema grammar

--- schema.sql
+++ schema.sql
@@ -4,22 +4,22 @@ CREATE TYPE event_state AS ENUM ('Cancelled', 'Finished', 'On-going', 'Due');
 CREATE TYPE notification_type AS ENUM ('Disabled event', 'Cancelled event', 'Join request', 'Accepted request', 'Declined request', 'Invite', 'Accepted invite', 'Declined Invite', 'New comment', 'New poll', 'Poll closed');
 CREATE TYPE report_motive AS ENUM ('Sexual harassment', 'Violence or bodily harm', 'Nudity or explicit content', 'Hate speech', 'Other');
 
-CREATE TABLE file {
+CREATE TABLE file (
     id SERIAL PRIMARY KEY,
     path TEXT UNIQUE NOT NULL,
     name TEXT NOT NULL
-};
+);
 
-CREATE TABLE location {
+CREATE TABLE location (
     id SERIAL PRIMARY KEY,
     address TEXT NOT NULL,
     city TEXT NOT NULL,
     coords TEXT NOT NULL,
     name TEXT,
     TYPE Country NOT NULL
-};
+);
 
-CREATE TABLE user {
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     username TEXT UNIQUE NOT NULL,
@@ -30,9 +30,9 @@ CREATE TABLE user {
     is_blocked BOOLEAN NOT NULL DEFAULT FALSE,
     TYPE gender NOT NULL,
     profile_picture INTEGER REFERENCES file ON DELETE SET NULL ON UPDATE CASCADE
-};
+);
 
-CREATE TABLE event {
+CREATE TABLE event (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
@@ -41,65 +41,65 @@ CREATE TABLE event {
     is_disabled BOOLEAN NOT NULL DEFAULT FALSE,
     TYPE event_state NOT NULL,
     cover_image INTEGER REFERENCES file ON DELETE SET NULL ON UPDATE CASCADE,
-    organizer INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+    organizer INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     location INTEGER REFERENCES location ON DELETE SET NULL ON UPDATE CASCADE 
-};
+);
 
-CREATE TABLE tag {
+CREATE TABLE tag (
     id SERIAL PRIMARY KEY,
     name TEXT UNIQUE NOT NULL
-};
+);
 
-CREATE TABLE event_tag {
+CREATE TABLE event_tag (
     event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
     tag INTEGER REFERENCES tag ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
     PRIMARY KEY (event, tag)
-};
+);
 
-CREATE TABLE attendance {
+CREATE TABLE attendance (
     event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE,
-    attendee INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+    attendee INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     PRIMARY KEY (event, attendee)
-};
+);
 
-CREATE TABLE attendance_request {
+CREATE TABLE attendance_request (
     id SERIAL PRIMERY KEY,
     event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE, 
-    user INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+    users INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     is_accepted BOOLEAN DEFAULT FALSE NOT NULL,
     is_invite BOOLEAN NOT NULL
-};
+);
 
-CREATE TABLE poll {
+CREATE TABLE poll (
     id SERIAL PRIMARY KEY,
     event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
-    creator INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+    creator INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
     date DATETIME DEFAULT NOW(),
     is_open BOOLEAN DEFAULT TRUE
-};
+);
 
-CREATE TABLE poll_option {
+CREATE TABLE poll_option (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     poll INTEGER REFERENCES poll ON DELETE CASCADE ON UPDATE CASCADE NOT NULL
-};
+);
 
-CREATE TABLE user_poll_option {
-    user INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+CREATE TABLE user_poll_option (
+    users INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     poll_option INTEGER REFERENCES poll_option ON DELETE CASCADE ON UPDATE 
-};
+);
 
-CREATE TABLE comment {
+CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
     event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
-    user INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE,
+    users INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE,
     body TEXT, 
     date DATETIME DEFAULT NOW() NOT NULL
-};
+);
 
-CREATE TABLE notification {
+CREATE TABLE notification (
     id SERIAL PRIMARY KEY,
     date DATETIME DEFAULT NOW() NOT NULL,
     TYPE notification_type NOT NULL,
@@ -108,18 +108,18 @@ CREATE TABLE notification {
     attendance_request INTEGER REFERENCES attendance_request ON DELETE CASCADE ON UPDATE CASCADE,
     poll INTEGER REFERENCES poll ON DELETE CASCADE ON UPDATE CASCADE,
     comment INTEGER REFERENCES comment ON DELETE CASCADE ON UPDATE CASCADE,
-    user INTEGER REFERENCES user ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
+    users INTEGER REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE NOT NULL,
     CONSTRAINT single_reference CHECK ((attendance_request IS NOT NULL AND event IS NULL AND comment IS NULL) OR (attendance_request IS NULL AND event IS NOT NULL AND comment IS NULL) OR (attendance_request IS NULL AND event IS NULL AND comment IS NOT NULL))
-};
+);
 
-CREATE TABLE report {
+CREATE TABLE report (
     id SERIAL PRIMARY KEY,
     date DATETIME DEFAULT NOW(),
     description TEXT,
     TYPE report_motive NOT NULL,
-    handled_by INTEGER REFERENCES user ON DELETE SET NULL ON UPDATE CASCADE, 
+    handled_by INTEGER REFERENCES users ON DELETE SET NULL ON UPDATE CASCADE, 
     reported_event INTEGER REFERENCES event ON DELETE CASCADE ON UPDATE CASCADE, 
-    reported_user INTEGER REFERENCES user ON DELETE CASCADE ON UPDATE CASCADE,
+    reported_user INTEGER REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
     reported_comment INTEGER REFERENCES comment ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT single_reference CHECK ((reported_event IS NOT NULL AND reported_user IS NULL AND reported_comment IS NULL) OR (reported_event IS NULL AND reported_user IS NOT NULL AND reported_comment IS NULL) OR (reported_event IS NULL AND reported_user IS NULL AND reported_comment IS NOT NULL))
-}
+)
