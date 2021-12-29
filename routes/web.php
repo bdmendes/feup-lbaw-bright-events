9b75c9c5fa820edbc88c7b8afaaff6fc2db870b5
David Preda <dtpreda30@gmail.com>
2021-12-29 23:46:18 +0000

    Fix route order

--- routes/web.php
+++ routes/web.php
@@ -33,12 +33,12 @@ Route::put('api/cards/{card_id}/', 'ItemController@create');
 Route::post('api/item/{id}', 'ItemController@update');
 Route::delete('api/item/{id}', 'ItemController@delete');
 
-//Users
-Route::get('{id}', 'User\UserController@show')->name('profile');
-
 // Authentication
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register');
+
+//Users
+Route::get('{id}', 'User\UserController@show')->name('profile');
