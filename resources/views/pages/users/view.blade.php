51cd029226e2439e99f94f44a486d6a257559a2a
David Preda <dtpreda30@gmail.com>
2022-01-01 12:19:21 +0000

    Add buttons to user profile

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -16,14 +16,27 @@
                 <div class="info w-100 px-3">
                     <span class="d-inline-flex align-items-center justify-content-between w-100">
                         <h1>{{ $user->name }}</h1>
-                        @if (Auth::check())
-                            @if (Auth::user()->id == $user->id)
-                                <a href="{{ route('editProfile', ['username' => Auth::user()->username]) }}"
-                                    class="text-white">
-                                    <i class="bi bi-pencil-square"></i>
-                                </a>
+                        <div class="d-flex flex-row">
+                            @if (Auth::check())
+                                @if (Auth::user()->is_admin)
+                                    <a href="" class="text-white m-3 text-decoration-none d-flex flex-column align-items-center">
+                                        <i class="bi bi-trash"></i>
+                                        <p>Remove</p>
+                                    </a>
+                                    <a href="" class="text-white m-3 text-decoration-none d-flex flex-column align-items-center">
+                                        <i class="bi bi-dash-circle"></i>
+                                        <p>Block</p>
+                                    </a>
+                                @endif
+                                @if (Auth::user()->id == $user->id)
+                                    <a href="{{ route('editProfile', ['username' => Auth::user()->username]) }}"
+                                        class="text-white m-3 text-decoration-none d-flex flex-column align-items-center">
+                                        <i class="bi bi-pencil-square"></i>
+                                        <p>Edit</p>
+                                    </a>
+                                @endif
                             @endif
-                        @endif
+                        </div>
                     </span>
                     <h3>{{ $user->username }}</h3>
                     @if (!is_null($user->bio))
