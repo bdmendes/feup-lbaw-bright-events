2f4f56afc715556d450bef74d6d892dc47ab317e
up201907216 <up201907216@edu.fe.up.pt>
2022-01-01 16:08:42 +0000

    Fix User and username position

--- resources/views/pages/users/view.blade.php
+++ resources/views/pages/users/view.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container-fluid w-75 bg-dark mt-1 text-light">
         <div class="row">
-            <div class="col-auto p-3 w-100">
+            <div class="col-auto d-flex flex-row p-3 w-100">
                 @if (is_null($user->profile_picture_id))
                     <img src="https://cdn.pixabay.com/photo/2017/08/10/02/05/tiles-shapes-2617112_960_720.jpg"
                         alt="Generic Profile Picture" class="mb-3 rounded-circle align-self-left"
@@ -15,7 +15,7 @@
                         class="mb-3 rounded-circle align-self-left" style="object-fit: cover; width: 300px; height: 300px;">
                 @endif
                 <div class="info w-100 px-3">
-                    <span class="d-inline-flex align-items-top justify-content-between w-100">
+                    <span class="d-inline-flex align-items-center justify-content-between w-100">
                         <div class="d-flex flex-column align-items-left">
                             <h1>{{ $user->name }}</h1>
                             <h3>{{ $user->username }}</h3>
