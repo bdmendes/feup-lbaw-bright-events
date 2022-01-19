c43526d1408c58f9475eaf593ba4754e0ef694b0
David Preda <dtpreda30@gmail.com>
2022-01-19 17:49:28 +0000

    Add trending events

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -12,9 +12,24 @@ class HomeController extends Controller
 {
     public function home()
     {
-        $events = Event::all()->take(3);
+        $events = $this->trendingEvents();
         $users = User::where('is_admin', 'false')->take(3)->get();
         return view('pages.home', compact('events', 'users'));
+    }
+
+    protected function trendingEvents()
+    {
+        $events = Event::all();
+        $events = $events->map(function ($item, $key) {
+            return ['score' => $item->score(), 'event' => $item];
+        });
+
+        
+        $events = $events->sort()->reverse()->take(3);
+        $events = $events->map(function ($item, $key) {
+            return $item['event'];
+        });
+        return $events;
     }
 
     public function redirect()
