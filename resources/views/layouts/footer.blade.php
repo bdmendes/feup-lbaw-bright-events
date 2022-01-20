84c75e968f59813d9db06abf2e26dd7a5929a343
David Preda <dtpreda30@gmail.com>
2022-01-20 23:33:39 +0000

    Fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,20 +1,20 @@
-<footer class="footer bg-light mt-auto" id="footer">
-    <div class="container-fluid bg-light mt-5 py-2 d-flex justify-content-between align-items-center">
-    <div class="d-inline-flex align-items-center">
-        @Bright Events 2021
+<footer class="footer mt-auto d-block" id="footer">
+    <div class="w-100 py-2 d-flex justify-content-between align-items-center">
+        <div class="d-inline-flex align-items-center">
+            @Bright Events 2021
+        </div>
+        <div class="p-0 d-inline-flex justify-content-end">
+            <a class="btn" href="{{ route('contacts') }}">
+                <h2 class="d-flex flex-no-wrap">Contact Us</h2>
+            </a>
+            <a class="btn" href="{{ route('faq') }}">
+                <h2>FAQ</h2>
+            </a>
+            <a class="btn" href="{{ route('about') }}">
+                <h2>About</h2>
+            </a>
+        </div>
     </div>
-    <div class="p-0 d-inline-flex justify-content-end">
-        <a class="btn" href="{{ route('contacts') }}">
-            <h2 class="d-flex flex-no-wrap">Contact Us</h2>
-        </a>
-        <a class="btn" href="{{ route('faq') }}">
-            <h2>FAQ</h2>
-        </a>
-        <a class="btn" href="{{ route('about') }}">
-            <h2>About</h2>
-        </a>
-    </div>
-</div>
 </footer>
 
 @yield('footer')
