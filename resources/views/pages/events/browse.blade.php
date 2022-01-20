0c860ae64988d95ba5c9cb9a15cd806e5701fabe
David Preda <dtpreda30@gmail.com>
2022-01-20 03:08:12 +0000

    Finish browse events

--- resources/views/pages/events/browse.blade.php
+++ resources/views/pages/events/browse.blade.php
@@ -3,124 +3,40 @@
 @section('title', 'events')
 
 @section('content')
-    <div class="d-flex">
-        <div class="col-md-3 border rounded py-2">
-
-            <h3 class="mb-3">Search</h3>
-            <form action="{{ route('browseEvents') }}" method="GET">
-                <div class="form-group border-bottom mb-5">
-                    <div class="input-group">
-                        <input style="font-size: 1em; border: none;" type="text" name="global"
-                            value="{{ $request->global ?? '' }}" autocomplete="off" name="search" class="form-control"
-                            placeholder="Enter a keyword">
-                        <span><button class="btn btn-outline-light" style="border:none; " type="submit"><i
-                                    class="bi bi-search" style="color: black;"></i></button></span>
-                    </div>
-            </form>
-        </div>
-
-        <h3 class="mb-3">Filter</h3>
-        <div class="mb-5 row gap-3">
-            <div data-bs-toggle="collapse" href="#filter-date">Date <i class="bi bi-chevron-down"></i></a>
-            </div>
-            <div class="collapse {{ $request->filled('begin_date') || $request->filled('end_date') ? 'show' : '' }}"
-                id="filter-date">
-                <form method="GET">
-                    <div>Begin <input id="date" value="{{ $request->begin_date }}" type="date" name="begin_date"
-                            onchange="this.form.submit()"></div>
-                    <div>End <input id="date" value="{{ $request->end_date }}" type="date" name="end_date"
-                            onchange="this.form.submit()"></div>
-                </form>
-            </div>
-
-            <div data-bs-toggle="collapse" href="#filter-organizer">Organizer <i class="bi bi-chevron-down"></i></a>
-            </div>
-            <div class="collapse {{ $request->filled('organizer') ? 'show' : '' }}" id="filter-organizer">
-                <form method="GET">
-                    <select name="organizer" onchange="this.form.submit()">
-                        <option value="" {{ !$request->filled('organizer') ? 'selected' : '' }}></option>
-                        @foreach ($users as $user)
-                            <option value="{{ $user->id }}"
-                                {{ $request->query('organizer') == $user->id ? 'selected' : '' }}>{{ $user->username }}
-                            </option>
-                        @endforeach
-                    </select>
-                </form>
-            </div>
-
-            <div data-bs-toggle="collapse" href="#filter-tag">Tag <i class="bi bi-chevron-down"></i></a>
-            </div>
-            <div class="mb-4 collapse {{ $request->filled('tag') ? 'show' : '' }}" id="filter-tag">
-                <form method="GET">
-                    <select name="tag" onchange="this.form.submit()">
-                        <option value="" {{ !$request->filled('tag') ? 'selected' : '' }}></option>
-                        @foreach ($tags as $tag)
-                            <option value="{{ $tag->name }}"
-                                {{ $request->query('tag') == $tag->name ? 'selected' : '' }}>{{ $tag->name }}
-                            </option>
-                        @endforeach
-                    </select>
-                </form>
-            </div>
-        </div>
-
-        <h3 class="mb-3">Order By</h3>
-        <div class="mb-5 d-flex row">
-            <div>
-                Title
-                <button
-                    onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'title', 'order' => 'ascending']) }}'"
-                    type="button"
-                    class="{{ $request->query('sort_by') == 'title' && $request->query('order') == 'ascending' ? 'active' : '' }}  btn btn-outline-info"><i
-                        class="bi bi-arrow-up" style="color: black;"></i></button>
-                <button
-                    onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'title', 'order' => 'descending']) }}'"
-                    type="button"
-                    class="{{ $request->query('sort_by') == 'title' && $request->query('order') == 'descending' ? 'active' : '' }} btn btn-outline-info"><i
-                        class="bi bi-arrow-down" style="color: black;"></i></button>
-            </div>
-            <div>
-                Date
-                <button
-                    onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'date', 'order' => 'ascending']) }}'"
-                    type="button"
-                    class="{{ $request->query('sort_by') == 'date' && $request->query('order') == 'ascending' ? 'active' : '' }} btn btn-outline-info"><i
-                        class="bi bi-arrow-up" style="color: black;"></i></button>
-                <button
-                    onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'date', 'order' => 'descending']) }}'"
-                    type="button"
-                    class="{{ $request->query('sort_by') == 'date' && $request->query('order') == 'descending' ? 'active' : '' }} btn btn-outline-info"><i
-                        class="bi bi-arrow-down" style="color: black;"></i></button>
-            </div>
-        </div>
-
+    <div class="d-xs-block d-sm-none d-md-none d-lg-none d-xl-none p-4">
+        @include('partials.events.search')
     </div>
+    <div class="d-flex justify-content-end">
 
-    <div class="col-md-9">
-        <div class="d-flex flex-row mb-4 align-items-center">
-            <div>
+        <div class="d-none d-sm-block col-sm-3 col-md-3 col-lg-3 rounded p-4 position-fixed" style="left:0">
+            @include('partials.events.search')
+        </div>
+
+        
+        <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9 p-4">
+            <div class="d-flex flex-row mb-4 align-items-center gap-4">
                 <h2>Browse Events</h2>
+                <a href="{{ route('createEvent') }}">
+                    <button type="button" class="btn btn-custom">Create Event</button>
+                </a>
             </div>
-            <div class="ms-auto">
-                <a href="{{ route('createEvent') }}"> <button type="button" class="btn btn-primary"
-                        style="font-size: 1em;">Create Event</button></a>
-            </div>
-        </div>
-        <div class="d-flex flex-wrap justify-content-stretch w-100 gap-4 mt-4">
-            @if ($events->isEmpty())
-                No events were found
-            @else
+            <div class="d-flex flex-wrap justify-content-stretch w-100 gap-4 mt-4">
+                @if ($events->isEmpty())
+                    <p>No events were found</p>
+                @else
                 @foreach ($events as $event)
-                    <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex flex-column flex-grow-1">
-                        @include('partials.events.verticalCard', ['event' => $event])
-                    </div>
+                <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 d-flex flex-column flex-grow-1">
+                    @include('partials.events.verticalCard', ['event' => $event])
+                </div>
                 @endforeach
-            @endif
-        </div>
-        @if ($events->links()->paginator->hasPages())
+                @endif
+            </div>
+            @if ($events->links()->paginator->hasPages())
             <div class="mt-4">
                 {!! $events->links() !!}
             </div>
-        @endif
-    </div>
-@endsection
+            @endif
+        </div>
+        @endsection
+</div>
+        
---
+++ resources/views/partials/events/search.blade.php
@@ -0,0 +1,108 @@
+<div class="mb-3">
+    
+    <h3 class="mb-4">Search</h3>
+    <form action="{{ route('browseEvents') }}" method="GET" class="mb-0">
+        <div class="d-flex flex-nowrap">
+            <input type="text" name="global" value="{{ $request->global ?? '' }}" autocomplete="off" name="search" class="input" placeholder="Enter a keyword">
+            <span>
+                <button class="btn btn-outline-light" style="border:none; " type="submit">
+                    <i class="bi bi-search" style="color: black;"></i>
+                </button>
+            </span>
+        </div>
+    </form>
+</div>
+
+
+<div class="d-flex w-100 justify-content-between align-items-center mb-3" data-bs-toggle="collapse" href="#filter">
+    <h2>Filter</h2>
+    <i class="bi bi-chevron-down"></i>
+</div>
+
+<div class="mb-4 collapse" id="filter">
+    <div class="d-flex w-100 justify-content-between" data-bs-toggle="collapse" href="#filter-date" onclick="flip('flip-date');" id="flip-date">
+        <h4>Date</h4>
+        <i class="bi bi-chevron-down"></i>
+    </div>
+    <div class="collapse {{ $request->filled('begin_date') || $request->filled('end_date') ? 'show' : '' }}"
+        id="filter-date">
+        <form method="GET">
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <h5>Begin</h5>
+                <input class="input" id="date" value="{{ $request->begin_date }}" type="date" name="begin_date" onchange="this.form.submit()">
+            </div>
+            <div class="d-flex justify-content-between align-items-center my-2">
+                <h5>End</h5> 
+                <input class="input" id="date" value="{{ $request->end_date }}" type="date" name="end_date" onchange="this.form.submit()">
+            </div>
+        </form>
+    </div>
+
+    <div data-bs-toggle="collapse" href="#filter-organizer">
+        <div class="d-flex w-100 justify-content-between" data-bs-toggle="collapse" href="#filter-organizer">
+        <h4>Organizer</h4>
+        <i class="bi bi-chevron-down"></i>
+    </div>
+    </div>
+    <div class="collapse {{ $request->filled('organizer') ? 'show' : '' }}" id="filter-organizer">
+        <form method="GET">
+            <select name="organizer" onchange="this.form.submit()">
+                <option value="" {{ !$request->filled('organizer') ? 'selected' : '' }}></option>
+                @foreach ($users as $user)
+                    <option value="{{ $user->id }}"
+                        {{ $request->query('organizer') == $user->id ? 'selected' : '' }}>{{ $user->username }}
+                    </option>
+                @endforeach
+            </select>
+        </form>
+    </div>
+
+    <div data-bs-toggle="collapse" href="#filter-tag">
+        <div class="d-flex w-100 justify-content-between" data-bs-toggle="collapse" href="#filter-tag">
+        <h4>Tag</h4>
+        <i class="bi bi-chevron-down"></i>
+    </div>
+    </div>
+    <div class="mb-4 collapse {{ $request->filled('tag') ? 'show' : '' }}" id="filter-tag">
+        <form method="GET">
+            <select name="tag" onchange="this.form.submit()">
+                <option value="" {{ !$request->filled('tag') ? 'selected' : '' }}></option>
+                @foreach ($tags as $tag)
+                    <option value="{{ $tag->name }}"
+                        {{ $request->query('tag') == $tag->name ? 'selected' : '' }}>{{ $tag->name }}
+                    </option>
+                @endforeach
+            </select>
+        </form>
+    </div>
+</div>
+
+<div class="d-flex w-100 justify-content-between align-items-center mb-3" data-bs-toggle="collapse" href="#order">
+    <h2>Order By</h2>
+    <i class="bi bi-chevron-down"></i>
+</div>
+
+<div class="mb-4 collapse" id="order">
+    <div class="d-flex align-items-center justify-content-between">
+        <h4>Title</h4>
+        <div>
+            <button onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'title', 'order' => 'ascending']) }}'" type="button" class="{{ $request->query('sort_by') == 'title' && $request->query('order') == 'ascending' ? 'active' : '' }}  btn btn-custom">
+                <i class="bi bi-arrow-up" style="color: white;"></i>
+            </button>
+            <button onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'title', 'order' => 'descending']) }}'" type="button" class="{{ $request->query('sort_by') == 'title' && $request->query('order') == 'descending' ? 'active' : '' }} btn btn-custom">
+                <i class="bi bi-arrow-down" style="color: white;"></i>
+            </button>
+        </div>
+    </div>
+    <div class="d-flex align-items-center justify-content-between my-2">
+        <h4>Date</h4>
+        <div>
+            <button onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'date', 'order' => 'ascending']) }}'" type="button" class="{{ $request->query('sort_by') == 'date' && $request->query('order') == 'ascending' ? 'active' : '' }} btn btn-custom">
+                <i class="bi bi-arrow-up" style="color: white;"></i>
+            </button>
+            <button onclick="location.href='{{ $request->fullUrlWithQuery(['sort_by' => 'date', 'order' => 'descending']) }}'" type="button" class="{{ $request->query('sort_by') == 'date' && $request->query('order') == 'descending' ? 'active' : '' }} btn btn-custom">
+                <i class="bi bi-arrow-down" style="color: white;"></i>
+            </button>
+        </div>
+    </div>
+</div>
